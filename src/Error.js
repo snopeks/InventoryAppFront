29b8de8386547f7819ebde97a59f33283b82f2d6
snopeks <creativesnopeks@gmail.com>
2018-05-02 21:55:39 -0700

    styles and misc

---
+++ src/Error.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Error extends Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <div>
+        <h3>Error!</h3>
+      </div>
+    )
+  }
+}
+
+export default Error;
