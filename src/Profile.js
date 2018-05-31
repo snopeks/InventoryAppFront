1e134d61916b337945d9f67ae46728ad02268b6f
snopeks <creativesnopeks@gmail.com>
2018-05-31 14:33:43 -0700

    list logged-in user households works

--- src/Profile.js
+++ src/Profile.js
@@ -1,11 +1,66 @@
 import React, {Component} from 'react'
+import $ from 'jquery-ajax';
+
 class Profile extends Component{
+  state = {
+    newHouseName: '',
+    households: []
+  }
+  handleHouseNameChange = (e) =>{
+    console.log('setting the house name!')
+    this.setState({newHouseName: e.target.value})
+  }
+  componentWillMount = () => {
+    console.log('mounting profile page!')
+    if(this.props.username){
+      $.ajax({
+        method: 'GET',
+        url: `http://localhost:3000/api/users/${this.props.username}`,
+      })
+      .then(res => {
+          console.log('returned user', res)
+          this.setState({households: res[0].households})
+      })
+    } else {
+      console.log('login to get a user back')
+    }
+  }
+  // componentDidUpdate = () => {
+  //   console.log('it updated!')
+  //   $.ajax({
+  //     method: "GET",
+  //     url:`http://localhost:3000/api/users/${this.props.username}`
+  //   })
+  //   .then(res => {
+  //     console.log(`updated user:`, res)
+  //     this.setState({households: res.households})
+  //   })
+  // }
+
   addHousehold = (e) => {
-    console.log('adding household!')
-    console.log(e.target)
+    e.preventDefault();
+    console.log('adding house!')
+    $.ajax({
+      method: "POST",
+      url: `http://localhost:3000/api/households`,
+      data: {
+        username: this.props.username,
+        name: this.state.newHouseName
+      }
+    })
+    .then(res => {
+      console.log('here is the returned house', res)
+      //take a copy of state to add the new house to state
+      const households = this.state.households;
+      households.push(res)
+      this.setState({households})
+    })
   }
   render(){
-    const households = this.props.households.map( household => <li className="list-group-item">{household}</li>)
+    const houseNames = this.state.households.map(household =>{ 
+      return <li key={household.name}className="list-group-item"><p>{household.name}</p></li>
+    })
+    console.log(houseNames)
     if(!this.props.isAuthed){
     return(
       <div>
@@ -14,24 +69,22 @@ class Profile extends Component{
     )} else {
       return(
         <div className="container">
-          <h2>Welcome {this.props.username}!</h2>
+          <h2>Welcome, {this.props.username}!</h2>
           <div className="row">
             <div className="col-sm-6 householdDiv">
-              <h3>Households</h3>
+              <h3>My Households</h3>
               <ul className="list-group">
-                {households}
+              {houseNames}
               </ul>
-              <p>Add Household</p>
-              <button onClick={this.addHousehold} data-toggle="modal" data-target="#addHouseholdModal" className="btn btn-light">+</button>
-              <form className="addHouseForm">
+              <form className="addHouseForm form-group" onSubmit={this.addHousehold}>
+                <h3>Add Household:</h3>
                 <label>Household name:</label>
-                <input type="text" placeholder="household name"></input>
-                <label>Members:</label>
-                <input type="text" placeholder="Who is part of this household?"/>
+                <input className="form-control" type="text" onChange={this.handleHouseNameChange} name="name" placeholder="household name"></input>
+                <button className="btn btn-light">ADD HOUSEHOLD</button>
               </form>
             </div>
             <div className="col-sm-6 householdDiv">
-              <h3>My Inventory</h3>
+              <h3>Inventory QuickView</h3>
             </div>
           </div>
         </div>
@@ -41,3 +94,4 @@ class Profile extends Component{
 }
 
 export default Profile;
+
