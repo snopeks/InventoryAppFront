a4b5118ac7d85dd038df7590a270c171b696073b
snopeks <creativesnopeks@gmail.com>
2018-05-30 19:24:52 -0700

    import css to indexjs, add inventory component, and pass auth state to header

---
+++ src/Inventory.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Inventory extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>Here is the inventory page!</p>
+            </div>
+        )
+    }
+}
+
+export default Inventory; 
